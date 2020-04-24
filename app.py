14632ab9e32b84ca30cdd63f933cc2c87344be88
UncleGoogle <mieszkoziemowit@gmail.com>
2020-04-25 01:27:14 +0200

    allow to refresh token

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ from flask import Flask, redirect, request, url_for
 CLIENT_ID = 929
 AUTH_OSU = '/auth/osu'
 AUTH_OSU_FINAL = '/auth/osu/redirect'
+AUTH_OSU_REFRESH = '/auth/osu/refresh'
 CLIENT_SECRET = os.environ['OSU_CLIENT_SECRET']
+
 
 app = Flask(__name__)
 
@@ -22,8 +24,7 @@ def root():
 
 @app.route(AUTH_OSU_FINAL)
 def auth_osu_final():
-    token = request.args.get('refresh_token')
-    print(token)
+    assert request.args.get('refresh_token')
     return "Finish. GOG Galaxy browser window should be closed"
 
 
@@ -33,33 +34,43 @@ def auth_osu():
     if code is None:
         return 'Error: No `code` received!'
     try:
-        auth_params = osu_auth(code)
+        auth_params = osu_auth_token(
+            grant_type='authorization_code',
+            code=code,
+            redirect_uri=request.host_url.strip('/') + AUTH_OSU
+        )
     except Exception as e:
         return 'Error: ' + repr(e)
     return redirect(url_for('auth_osu_final', **auth_params))
 
 
-def osu_auth(code):
-    """Exchanges oauth `code` to `access_token`. Exemplary response:
+@app.route(AUTH_OSU_REFRESH)
+def auth_osu_refresh():
+    tkn = request.args.get('refresh_token')
+    if tkn is None:
+        return 'Error: No `refresh_token` received!'
+    return osu_auth_token(
+        grant_type='refresh_token',
+        refresh_token=tkn
+    )
+
+
+def osu_auth_token(**custom):
+    """Gets `access_token`. Exemplary response:
     {
         "token_type":"Bearer",
         "expires_in":86400,  # token lifetime 24h
         "access_token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5MjkiLCJqdGkiOiIzYThlZWZkYjA2YTc2ZTgxOWNmYjAyNTY5Y2EzYzA0ZWFiZDdkM2I3N2U4NzU1MTZmNTJkMWIyY2I1OTlhMzJmM2I4MjVmNzcyYTB1ODhlYyIsImlhdCI6MTU4NjExOTk3NSwibmJmIjoxNTg2MTE5OTc1LCJleHAiOjE1ODYyMDYzNzUsInN1YiI6IjE2NTE3MTE2Iiwic2NvcGVzIjpbImlkZW50aWZ5Il19.xxx",
         "refresh_token":"xxxxx"
     }
-    Decoded from token JWT: {'sub': 16517116}  # is it user id?
     """
-    print('Authorizing with code...')
     params = {
-        'grant_type': 'authorization_code',
-        'code': code,
         'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-        'redirect_uri': request.host_url.strip('/') + AUTH_OSU
+        'client_secret': CLIENT_SECRET
     }
+    params.update(custom)
     url = 'http://osu.ppy.sh/oauth/token'
     response = requests.post(url, json=params)
-    print(response.status_code)
     response.raise_for_status()
     return response.json()
 
